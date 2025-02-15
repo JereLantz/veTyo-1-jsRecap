0e04da758b5ec9977814613d092d57712d9dd452
Jere Lantz <lantz.jere@gmail.com>
2025-02-15 10:38:52 +0200

    part 1 import ja export toimivat

---
+++ modules/main.js
@@ -0,0 +1,6 @@
+//@ts-check
+
+import * as math from "./math.js"
+
+console.log(math.add(2,2))
+console.log(math.substract(5, 3))
---
+++ modules/math.js
@@ -0,0 +1,19 @@
+//@ts-check
+
+/**
+ * @param {number} a
+ * @param {number} b 
+ * @returns {number}
+ */
+export function add(a,b){
+    return a + b
+}
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+export function substract(a,b){
+    return a - b
+}
